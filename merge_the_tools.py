86269ced9ccba044bfac97b4dfa5f6ae0a632bde
malik4318p <malikismi3@gmail.com>
2025-11-07 16:36:45 +0500

    renamed the file

--- merge_the_tools.py
+++ merge_the_tools.py
@@ -1,24 +1,24 @@
 def merge_the_tools(string, k):
     length = len(string)
     # substring_length = length//k
-    number_of_substrings= length//k
+    number_of_substrings = length//k
     if length % k != 0:
-        number_of_substrings+=1
+        number_of_substrings += 1
 
     starting_index = 0
     # substring_length=k
-    ending_index=k
+    ending_index = k
     for i in range(number_of_substrings):
         printed_arr = ""
         dummy_string = string[starting_index:ending_index]
         for j in range(starting_index, ending_index):
             if string[j] not in printed_arr:
-                printed_arr+=string[j]
+                printed_arr += string[j]
         print(printed_arr)
         starting_index += k
         ending_index += k
         if ending_index > length:
-            ending_index=length
+            ending_index = length
 
 
 if __name__ == '__main__':
